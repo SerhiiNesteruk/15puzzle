286bce41fa40f37940f996a4f8b0cc18247d6ae2
ns <serhii.nesteruk@gmail.com>
2019-06-05 00:35:42 +0300

    always generate solvable board

--- src/main/scala/com/wix/puzzle/FifteenPuzzle.scala
+++ src/main/scala/com/wix/puzzle/FifteenPuzzle.scala
@@ -6,39 +6,44 @@ sealed trait GameState
 case class PlayState(board: Vector[Int], hole: Point) extends GameState
 case object TerminateState extends GameState
 
-object PlayState {
-  val holeValue: Int = -100
-}
-
 case class Point(x: Int, y: Int)
 
 class FifteenPuzzle(dimension: Int = 4) {
 
-  import PlayState._
+  import FifteenPuzzle._
 
   def init: PlayState = {
-    val board = Random.shuffle((1 until dimension * dimension).toVector) ++ Vector(holeValue)
-    PlayState(board, Point(dimension - 1, dimension - 1))
+    val randomBoard = Random.shuffle((1 until dimension * dimension).toVector) ++ Vector(holeValue)
+    val correctBoard = if (isSolvable(randomBoard, dimension)) randomBoard else fixImpossibleBorad(randomBoard)
+    PlayState(correctBoard, Point(dimension - 1, dimension - 1))
+  }
+
+  private def fixImpossibleBorad(board: Vector[Int]) = {
+    val arr = board.toArray
+    val tmp = board(0)
+    arr(0) = board(1)
+    arr(1) = tmp
+    arr.toVector
   }
 
   def up(playState: PlayState): PlayState = {
     if (playState.hole.y == 0) playState
-    else switch(playState, playState.hole.copy(y = playState.hole.y - 1))
+    else swap(playState, playState.hole.copy(y = playState.hole.y - 1))
   }
 
   def down(playState: PlayState): PlayState = {
     if (playState.hole.y == dimension - 1) playState
-    else switch(playState, playState.hole.copy(y = playState.hole.y + 1))
+    else swap(playState, playState.hole.copy(y = playState.hole.y + 1))
   }
 
   def left(playState: PlayState): PlayState = {
     if (playState.hole.x == 0) playState
-    else switch(playState, playState.hole.copy(x = playState.hole.x - 1))
+    else swap(playState, playState.hole.copy(x = playState.hole.x - 1))
   }
 
   def right(playState: PlayState): PlayState = {
     if (playState.hole.x == dimension - 1) playState
-    else switch(playState, playState.hole.copy(x = playState.hole.x + 1))
+    else swap(playState, playState.hole.copy(x = playState.hole.x + 1))
   }
 
   def checkWin(playState: PlayState): Boolean = {
@@ -53,7 +58,7 @@ class FifteenPuzzle(dimension: Int = 4) {
     checkWin(playState.board.toList, 0)
   }
 
-  private def switch(playState: PlayState, point: Point): PlayState = {
+  private def swap(playState: PlayState, point: Point): PlayState = {
     val tmp = playState.board(playState.hole.y * dimension + playState.hole.x)
     val st1 = playState.board.updated(playState.hole.y * dimension + playState.hole.x, playState.board(point.y * dimension + point.x))
     val newState = st1.updated(point.y * dimension + point.x, tmp)
@@ -64,12 +69,44 @@ class FifteenPuzzle(dimension: Int = 4) {
     val maxLen = dimension * dimension.toString.length
 
     gameState.board.zipWithIndex.foreach { case (value, idx) =>
-      val displayValue = if (value == PlayState.holeValue) "_" else value.toString
+      val displayValue = if (value == holeValue) "_" else value.toString
       val spaces = maxLen - displayValue.length - 1
       print(displayValue + " " * spaces)
       if ((idx + 1) % dimension == 0) println()
     }
 
     println()
+  }
+
+}
+
+object FifteenPuzzle {
+  val holeValue: Int = -100
+
+  def isSolvable(board: Vector[Int], dimension: Int): Boolean = {
+    if (dimension % 2 == 0) isEvenSolvable(board: Vector[Int])
+    else isOddSolvable(board: Vector[Int])
+  }
+
+  private def isEvenSolvable(board: Vector[Int]): Boolean = {
+    //we assume that hole is always at bottom
+    numOfInversions(board) % 2 == 0
+  }
+
+  private def isOddSolvable(board: Vector[Int]): Boolean = {
+    numOfInversions(board) % 2 == 0
+  }
+
+  private def numOfInversions(board: Vector[Int]): Int = {
+    var inversions = 0
+    for(
+      i <- board.indices if board(i) != holeValue;
+      j <- i until board.size if board(j) != holeValue
+    ) {
+      if (board(i) > board(j)) {
+        inversions = inversions + 1
+      }
+    }
+    inversions
   }
 }
--- src/test/scala/com/wix/puzzle/FifteenPuzzleSpec.scala
+++ src/test/scala/com/wix/puzzle/FifteenPuzzleSpec.scala
@@ -3,18 +3,19 @@ package com.wix.puzzle
 import org.scalatest.{FlatSpec, Matchers}
 
 class FifteenPuzzleSpec extends FlatSpec with Matchers {
+  import FifteenPuzzle._
   val dimension = 4
   val puzzle = new FifteenPuzzle(dimension)
 
   "FifteenPuzzle" should "correct initialize board" in {
     val initState = puzzle.init
-    val allValues = (PlayState.holeValue :: (1 until dimension*dimension).toList).toSet
+    val allValues = (holeValue :: (1 until dimension*dimension).toList).toSet
     initState.board should contain theSameElementsAs allValues
   }
 
   val testBoard = Vector(
     1, 2, 3, 4,
-    5, 6, PlayState.holeValue, 8,
+    5, 6, holeValue, 8,
     9, 10, 11, 12,
     13, 14, 15, 7
   )
@@ -24,7 +25,7 @@ class FifteenPuzzleSpec extends FlatSpec with Matchers {
   it should "correctly move up if possible" in {
     val newBoard = puzzle.up(testState)
     newBoard.board should contain theSameElementsInOrderAs Vector(
-      1, 2, PlayState.holeValue, 4,
+      1, 2, holeValue, 4,
       5, 6, 3, 8,
       9, 10, 11, 12,
       13, 14, 15, 7
@@ -37,7 +38,7 @@ class FifteenPuzzleSpec extends FlatSpec with Matchers {
     newBoard.board should contain theSameElementsInOrderAs Vector(
       1, 2, 3, 4,
       5, 6, 11, 8,
-      9, 10, PlayState.holeValue, 12,
+      9, 10, holeValue, 12,
       13, 14, 15, 7
     )
     newBoard.hole shouldBe testHole.copy(y = testHole.y + 1)
@@ -47,7 +48,7 @@ class FifteenPuzzleSpec extends FlatSpec with Matchers {
     val newBoard = puzzle.left(testState)
     newBoard.board should contain theSameElementsInOrderAs Vector(
       1, 2, 3, 4,
-      5, PlayState.holeValue, 6, 8,
+      5, holeValue, 6, 8,
       9, 10, 11, 12,
       13, 14, 15, 7
     )
@@ -58,7 +59,7 @@ class FifteenPuzzleSpec extends FlatSpec with Matchers {
     val newBoard = puzzle.right(testState)
     newBoard.board should contain theSameElementsInOrderAs Vector(
       1, 2, 3, 4,
-      5, 6, 8, PlayState.holeValue,
+      5, 6, 8, holeValue,
       9, 10, 11, 12,
       13, 14, 15, 7
     )
@@ -67,7 +68,7 @@ class FifteenPuzzleSpec extends FlatSpec with Matchers {
 
   it should "not change state if move up is not possible" in {
     val cornerBoard = Vector(
-      1, 2, PlayState.holeValue, 4,
+      1, 2, holeValue, 4,
       5, 6, 3, 8,
       9, 10, 11, 12,
       13, 14, 15, 7
@@ -84,7 +85,7 @@ class FifteenPuzzleSpec extends FlatSpec with Matchers {
       1, 2, 15, 4,
       5, 6, 3, 8,
       9, 10, 11, 12,
-      13, 14, PlayState.holeValue, 7
+      13, 14, holeValue, 7
     )
     val conrnerHole = Point(2, 3)
     val oldState = PlayState(cornerBoard, conrnerHole)
@@ -98,7 +99,7 @@ class FifteenPuzzleSpec extends FlatSpec with Matchers {
     val cornerBoard = Vector(
       1, 2, 15, 4,
       5, 6, 3, 8,
-      PlayState.holeValue, 10, 11, 12,
+      holeValue, 10, 11, 12,
       13, 14, 9, 7
     )
     val conrnerHole = Point(0, 2)
@@ -113,7 +114,7 @@ class FifteenPuzzleSpec extends FlatSpec with Matchers {
     val cornerBoard = Vector(
       1, 2, 15, 4,
       5, 6, 3, 8,
-      12, 10, 11, PlayState.holeValue,
+      12, 10, 11, holeValue,
       13, 14, 9, 7
     )
     val conrnerHole = Point(3, 2)
@@ -129,14 +130,14 @@ class FifteenPuzzleSpec extends FlatSpec with Matchers {
       1, 2, 3, 4,
       5, 6, 7, 8,
       9, 10, 11, 12,
-      13, 14, 15, PlayState.holeValue
+      13, 14, 15, holeValue
     )
     val hole = Point(3, 3)
     val winState = PlayState(board, hole)
     puzzle.checkWin(winState) shouldBe true
 
     val notWinBoard = Vector(
-      PlayState.holeValue, 1, 2, 3,
+      holeValue, 1, 2, 3,
       4, 5, 6, 7,
       8, 9, 10, 11,
       12, 13, 14, 15
@@ -144,5 +145,64 @@ class FifteenPuzzleSpec extends FlatSpec with Matchers {
     val notWinHole = Point(0, 0)
     val notWinState = PlayState(notWinBoard, notWinHole)
     puzzle.checkWin(notWinState) shouldBe false
+  }
+
+  it should "check if board is solvable for odd dimensions" in {
+    val boardSolvable = Vector(
+      1, 2, holeValue,
+      4, 5, 3,
+      7, 8, 6
+    )
+
+    val boardImpossible = Vector(
+      1, 2, 3,
+      4, 5, 6,
+      8, 7, holeValue
+    )
+
+    val winBoard = Vector(
+      1, 2, 3,
+      4, 5, 6,
+      7, 8, holeValue
+    )
+
+    isSolvable(boardSolvable, 3) shouldBe true
+    isSolvable(boardImpossible, 3) shouldBe false
+    isSolvable(winBoard, 3) shouldBe true
+  }
+
+  it should "check if board is solvable for even dimensions" in {
+    val boardSolvable = Vector(
+      1, 2, 3, 4,
+      5, 6, 7, holeValue,
+      9, 10, 11, 8,
+      13, 14, 15, 12
+    )
+
+    val boardImpossible = Vector(
+      1, 2, 3, 4,
+      5, 6, 7, 8,
+      9, 10, 11, 12,
+      13, 15, 14, holeValue
+    )
+
+    val winBoard = Vector(
+      1, 2, 3, 4,
+      5, 6, 7, 8,
+      9, 10, 11, 12,
+      13, 14, 15, holeValue
+    )
+
+    isSolvable(boardSolvable, 3) shouldBe true
+    isSolvable(boardImpossible, 3) shouldBe false
+    isSolvable(winBoard, 3) shouldBe true
+  }
+
+  it should "always generate solvable board" in {
+    val numOfGenerations = 10
+    (0 to numOfGenerations).foreach {_ =>
+      val newGame = puzzle.init
+      isSolvable(newGame.board, dimension) shouldBe true
+    }
   }
 }
